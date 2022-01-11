a42cd6644051ae564476107a745c58f71df9365d
Mulyono100 <Mulyono101299@gmail.com>
2022-01-12 06:13:13 +0700

    first commit

---
+++ blog.js
@@ -0,0 +1,3 @@
+const text = document.getElementsByClassName("blog");
+text.innerHTML =
+  "Design begins after I begin to think about how to present an experience most successfully, whether a button I put in can solve a problem. The only point in design is not ui design, if the user does not have a good experience at the end of the product, the design will be considered unsuccessful in my opinion.";
